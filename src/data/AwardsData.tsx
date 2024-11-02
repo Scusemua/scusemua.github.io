d8ec5b54ed3e9c2b85123855a711f44a19242773
Scusemua <benrcarver@gmail.com>
2024-11-02 15:44:26 -0400

    Updated award icons

--- src/data/AwardsData.tsx
+++ src/data/AwardsData.tsx
@@ -3,6 +3,12 @@ import {ReactNode} from "react";
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import {Avatar} from "@mui/material";
 import * as React from "react";
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import TrophyIcon from "@icons/trophy_icon";
+import ScholarshipIcon from "@icons/scholarship";
+import FellowshipIcon from "@icons/fellowship";
+import AwardCertificateIcon from "@icons/award_certificate";
 
 export interface AwardData {
     name: string;
@@ -18,6 +24,9 @@ const DistinguishedAcademicAchievement2020: AwardData = {
     date: "May 2020",
     description: "For excellent academic performance.",
     additionalInfoHeader: <b>Courses Taken During Award Period:</b>,
+    icon: (<Avatar style={{width: 50, height: 50}}>
+        <TrophyIcon fill={"#fff"} stroke={"#fff"} transform="scale(1.25, 1.25)"/>
+    </Avatar>),
     additionalInfo: [
         "CS 571 - Operating Systems (Graduate Level)",
         "CS 550 - Database Systems (Graduate Level)",
@@ -33,8 +42,8 @@ const DistinguishedUndergradResearch: AwardData = {
     name: "Distinguished Undergraduate Research",
     date: "May 2020",
     description: "For outstanding research contributions as an undergraduate student.",
-    icon: (<Avatar style={{width: 45, height: 45}}>
-        <EmojiEventsIcon fill={"#fff"} stroke={"#fff"} transform="scale(1.1, 1.1)"/>
+    icon: (<Avatar style={{width: 50, height: 50}}>
+        <AwardCertificateIcon fill={"#fff"} transform="scale(1.35, 1.35)"/>
     </Avatar>)
 }
 
@@ -43,6 +52,9 @@ const DistinguishedAcademicAchievement2021: AwardData = {
     date: "May 2021",
     description: "For excellent academic performance.",
     additionalInfoHeader: <b>Courses Taken During Award Period (All Graduate Level):</b>,
+    icon: (<Avatar style={{width: 50, height: 50}}>
+        <TrophyIcon fill={"#fff"} stroke={"#fff"} transform="scale(1.25, 1.25)"/>
+    </Avatar>),
     additionalInfo: [
         "CS 799 - Master's Thesis",
         "CS 747 - Deep Learning",
@@ -61,15 +73,18 @@ const GMUPresidentialScholar: AwardData = {
     name: "GMU CS Presidential Scholarship",
     date: "May 2021",
     description: "Prestigious, merit-based scholarship awarded to at-most 2 students per academic year (within a PhD program). Requires a strong faculty nomination.",
-    icon: (<Avatar style={{width: 45, height: 45}}>
-        <EmojiEventsIcon fill={"#fff"} stroke={"#fff"} transform="scale(1.1, 1.1)"/>
+    icon: (<Avatar style={{width: 50, height: 50}}>
+        <FellowshipIcon fill={"#fff"} stroke={"#fff"} transform="scale(1.5, 1.5)"/>
     </Avatar>)
 }
 
 const VLDB2023NSFTravelGrant: AwardData = {
     name: "VLDB 2023 NSF Travel Grant",
     date: "August 2023",
-    description: "Funding to attend VLDB 2023 and present a paper."
+    description: "Funding to attend VLDB 2023 and present a paper.",
+    icon: (<Avatar style={{width: 50, height: 50}}>
+        <AirplanemodeActiveIcon fill={"#fff"} stroke={"#fff"} transform="scale(2, 2)"/>
+    </Avatar>)
 }
 
 export const AwardsData: AwardData[] = [
