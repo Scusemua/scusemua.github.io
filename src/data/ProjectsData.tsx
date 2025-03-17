f0e3cce554901099d865e147fe7e741c7af1b28d
Scusemua <benrcarver@gmail.com>
2025-03-17 16:50:30 -0400

    Updated some project descriptions

--- src/data/ProjectsData.tsx
+++ src/data/ProjectsData.tsx
@@ -3,6 +3,7 @@ import infinistore_logo from "@images/logos/infinistore_logo.webp";
 import obliv_inf_store_logo from "@images/logos/oblivious_infinistore_logo.webp";
 import lfs_logo from "@images/logos/lambda_fs_logo.webp";
 import jupyter_naas from "@images/logos/jupyter_naas_logo.webp";
+import distributed_dl_training from "@images/logos/distributed_dl_training_logo.png";
 import faas_platform_logo from "@images/logos/faas_platforms_logo.webp";
 import LambdaFSArchitecture from "@data/architecture_diagrams/lambda_fs_arch";
 import WukongArchitecture from "@data/architecture_diagrams/wukong_arch";
@@ -85,14 +86,17 @@ export const LambdaFSProject: Project = {
 }
 
 export const JupyterNotebookProject: Project = {
-    name: "Jupyter NaaS",
-    description: "I'm working on a Jupyter Notebooks-as-a-Service (NaaS) platform that is optimized for providing maximum interactivity for users. The platform uses a novel designed that we developed to enable support for new classes of AI/ML applications and workloads.",
+    // name: "Jupyter NaaS",
+    name: "Distributed Deep Learning Training",
+    description: "I'm working on a platform optimized for multi-tenant distributed deep learning training workloads. The platform uses a novel design to enhance resource utilization and reduce JCT.",
+    // description: "I'm working on a Jupyter Notebooks-as-a-Service (NaaS) platform that is optimized for providing maximum interactivity for users. The platform uses a novel designed that we developed to enable support for new classes of AI/ML applications and workloads.",
     extendedDescription: "The platform supports detailed, real-time metric and log collection, visualization, aggregation, and monitoring by integrating with industry-standard frameworks and technologies, including Prometheus, Promtail, Loki, and Grafana.",
     venue: "",
-    repo_url: "https://github.com/Scusemua/Jupyter-NaaS-Dashboard",
+    // repo_url: "https://github.com/Scusemua/Jupyter-NaaS-Dashboard",
+    repo_url: "",
     project_website_url: "",
-    image: jupyter_naas,
-    keywords: ["serverless", "deep learning", "jupyter notebooks", "gpu scheduling", "platforms"],
+    image: distributed_dl_training, // jupyter_naas,
+    keywords: ["serverless", "deep learning", "gpu scheduling", "platforms"], // "jupyter notebooks"
     builtWith: ["Kubernetes", "Docker", "Golang", "Python"],
     arxiv_links: [],
     status: 'ongoing',
