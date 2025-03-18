3e3e3ea08040071cef4e44b35460ad61b1e956d1
Scusemua <benrcarver@gmail.com>
2025-03-18 19:31:56 -0400

    Updated skills.

--- src/data/SkillsData.tsx
+++ src/data/SkillsData.tsx
@@ -20,28 +20,28 @@ export interface Skills {
 export const CloudPlatformSkills: Skills = {
     category: SkillCategory.Platforms,
     icon: <CloudIcon fontSize={"inherit"} style={{fontSize: "24pt"}}/>,
-    skills: ['Google Cloud Platform', 'Azure Cloud', 'Amazon Web Services', 'IBM Cloud', '{{break}}', 'EC2', 'ECS', 'EKS', 'GKE', 'Lambda', 'S3', 'VPC', 'IAM', 'Fargate']
+    skills: ['Google Cloud Platform', 'Azure Cloud', 'Amazon Web Services', 'IBM Cloud', '{{break}}', 'EC2', 'ECS',
+        'EKS', 'GKE', 'Lambda', 'S3', 'VPC', 'IAM', 'Fargate']
 }
 
 export const ToolSkills: Skills = {
     category: SkillCategory.Tools,
     icon: <BuildIcon fontSize={"inherit"} style={{fontSize: "24pt"}}/>,
-    skills: ['Docker', 'Kubernetes', 'Docker Compose', 'Docker Swarm', 'Loki/Promtail', 'Visual Studio', 'Ansible', 'Prometheus', 'IntelliJ', 'WebStorm', 'Grafana', 'Traefik', 'Goland', 'Linux', 'HDFS']
-    // skills: ["Docker", "Docker Compose", "Docker Swarm", "Kubernetes", "Grafana", "Loki/Promtail", "Prometheus", "Kafka", "Traefik", "Visual Studio", "IntelliJ", "YourKit Profiler", "RedGate Profilers", "Goland", "WebStorm", "Linux"]
+    skills: ['Docker', 'Kubernetes', 'Docker Compose', 'Docker Swarm', 'MySQL Cluster NDB', 'Visual Studio', 'Ansible',
+        'Prometheus', 'IntelliJ', 'WebStorm', 'Grafana', 'Traefik', 'Goland', 'Promtail', 'Linux', 'HDFS', 'Loki',
+        'Redis', 'MySQL', 'PyTorch']
 }
 
 export const ProgrammingLanguagesSkills: Skills = {
     category: SkillCategory.Languages,
     icon: <TerminalIcon fontSize={"inherit"} style={{fontSize: "24pt"}}/>,
-    skills: ['JavaScript/TypeScript', 'Golang', 'Python', 'Java', 'C++', 'C#', 'C']
-    // skills: ["Java", "Golang", "Python", "C++", "C", "C#", "JavaScript/TypeScript"]
+    skills: ['JavaScript/TypeScript', 'Golang', 'Python', 'Java', 'C++', 'C#', 'C', 'SQL']
 }
 
 export const RelevantCoursework: Skills = {
     category: SkillCategory.Coursework,
     icon: <SchoolIcon fontSize={"inherit"} style={{fontSize: "24pt"}}/>,
     skills: ['Large-Scale Optimization for Deep Learning', 'Advanced Artificial Intelligence', 'Concurrent & Distributed Systems', 'Object Oriented Software Design', 'Artificial Intelligence', 'Computer Architecture', 'Systems Programming', 'Distributed Systems', 'Mobile Development', 'Operating Systems', 'Computer Networks', 'Programming in C', 'Machine Learning', 'Graph Algorithms', 'Data Structures', 'Convex Analysis', 'Linear Algebra', 'Data Analytics', 'Deep Learning', 'Algorithms', 'Databases', 'Compilers']
-    // skills: ["Programming in C", "Linear Algebra", "Data Structures", "Operating Systems", "Computer Networks", "Databases", "Computer Architecture", "Algorithms", "Systems Programming", "Compilers", "Object Oriented Software Design", "Machine Learning", "Artificial Intelligence", "Advanced Artificial Intelligence", "Graph Algorithms", "Deep Learning", "Data Analytics", "Mobile Development", "Convex Analysis", "Large-Scale Optimization for Deep Learning", "Distributed Systems", "Concurrent & Distributed Systems"]
 }
 
 export const SkillsData: Skills[] = [
