352320e4715e12b09be52bc1f0159cd943015673
Scusemua <benrcarver@gmail.com>
2025-03-30 15:25:22 -0400

    Decreased default rotation factor

--- src/components/Context/SettingsContext.tsx
+++ src/components/Context/SettingsContext.tsx
@@ -11,7 +11,7 @@ const SettingsContext = createContext<SettingsContextType | undefined>(
     undefined
 );
 
-export const DefaultRotationMultiplier: number = 6;
+export const DefaultRotationMultiplier: number = 5;
 export const MaxRotationMultiplier: number = 100.0;
 
 // Provider component
