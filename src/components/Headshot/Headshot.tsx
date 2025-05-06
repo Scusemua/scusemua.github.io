523ced2a65bc1461fb2901e9eb6a9a3fcc61111a
Scusemua <benrcarver@gmail.com>
2025-05-06 14:43:46 -0400

    Refactored clouds component

--- src/components/Headshot/Headshot.tsx
+++ src/components/Headshot/Headshot.tsx
@@ -5,7 +5,7 @@ import React, {forwardRef} from "react";
 import Image from "next/image";
 
 import {PersonalData} from '@data/PersonalData';
-import {Box, Button, Stack, Tooltip, useMediaQuery} from "@mui/material";
+import {Button, Stack, Tooltip, useMediaQuery} from "@mui/material";
 
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -19,8 +19,9 @@ import DownloadIcon from '@mui/icons-material/Download';
 import theme from "@src/app/theme";
 import {BubbleContext} from "@src/providers/BubbleContext";
 import Link from "next/link";
-import {Info, SettingsRounded} from "@mui/icons-material";
+import {SettingsRounded} from "@mui/icons-material";
 import SettingsModal from "@src/components/Modals/SettingsModal";
+import Clouds from "@src/components/Effects/Clouds";
 
 interface HeadshotProps {
 }
@@ -374,8 +375,6 @@ const Headshot = forwardRef<HTMLInputElement, HeadshotProps>((_props: HeadshotPr
         }
     };
 
-    console.log(`settingsModalOpen: ${settingsModalOpen}`)
-
     return (
         <div className={styles.headshot} ref={ref} key={"headshot_layout_wrapper"}
              style={{marginTop: getTopMargin(), marginLeft: 'auto', marginRight: 'auto'}}>
@@ -386,6 +385,14 @@ const Headshot = forwardRef<HTMLInputElement, HeadshotProps>((_props: HeadshotPr
             {mq_xs && <ExtraSmallHeadshotLayout onClickSettingsButton={() => setSettingsModalOpen(true)}/>}
             {!mq_xl && <HeaderButtons/>}
             <SettingsModal open={settingsModalOpen} onClose={() => setSettingsModalOpen(false)}/>
+            {/*<div className={styles.project_divider}>*/}
+            {/*    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"*/}
+            {/*         preserveAspectRatio="none">*/}
+            {/*        <path*/}
+            {/*            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"*/}
+            {/*            className="shape-fill"></path>*/}
+            {/*    </svg>*/}
+            {/*</div>*/}
         </div>
     );
 });
