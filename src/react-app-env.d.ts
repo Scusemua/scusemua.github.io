69e3ae461368b91f79209c64504f6b06e3692713
Scusemua <benrcarver@gmail.com>
2026-02-16 20:02:11 -0500

    Trying to fix CI

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,39 @@
-/// <reference types="react-scripts" />
+declare module '*.module.scss' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.module.css' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.scss' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.css' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.webp' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.svg' {
+    const src: string;
+    export default src;
+}
